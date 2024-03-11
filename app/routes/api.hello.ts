ebf239649883317e62640952d225559c5f9cf0f8
khair ali <khair@nine-15.com>
2024-03-11 19:10:21 +0500

    add cors

--- app/routes/api.hello.ts
+++ app/routes/api.hello.ts
@@ -2,21 +2,7 @@ import { ActionFunction, json } from "@remix-run/node"; // or cloudflare/deno
 import { setCustomerNotified, findAll, subscribeProduct } from "~/services/customer-subscriber.service";
 import { authenticate } from "~/shopify.server";
 
-function withCors(request: any) {
-    request.headers.append('Access-Control-Allow-Origin', '*');
-
-    request.headers.append(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, referer-path'
-    );
-}
-
 export const action: ActionFunction = async ({ request }) => {
-    withCors(request);
-    return new Response(null, {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE',
-        },
-    });
+    const { cors } = await import("remix-utils/cors");
+    return json({ nice: "jokess" });
 };
