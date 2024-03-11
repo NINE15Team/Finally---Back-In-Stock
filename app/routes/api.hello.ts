fcce15cd4994095de0be1ff95bd3521488973a2c
khair ali <khair@nine-15.com>
2024-03-11 19:40:46 +0500

    cors issue

--- app/routes/api.hello.ts
+++ app/routes/api.hello.ts
@@ -1,8 +1,13 @@
-import { ActionFunction, json } from "@remix-run/node"; // or cloudflare/deno
+import { ActionFunction, LoaderFunction, json } from "@remix-run/node"; // or cloudflare/deno
 import { setCustomerNotified, findAll, subscribeProduct } from "~/services/customer-subscriber.service";
 import { authenticate } from "~/shopify.server";
+import { cors } from "remix-utils/cors";
+
+export const loader: LoaderFunction = async ({ request }) => {
+    return await cors(request, json({}));
+};
+
 
 export const action: ActionFunction = async ({ request }) => {
-    const { cors } = await import("remix-utils/cors");
     return await cors(request, json({ nice: "test" }, 200));
 };
