50622452b02ea6c4fb7ac72f2d1bae41c78f3a37
khair ali <khair@nine-15.com>
2024-03-16 02:35:38 +0500

    cors

--- extensions/bis-client/assets/back-in-stock.js
+++ extensions/bis-client/assets/back-in-stock.js
@@ -35,7 +35,7 @@ class BackInStock extends HTMLElement {
         alert('Invalid Email');
         return false;
       }
-      const API_URL = "https://fixed-crash-for-breast.trycloudflare.com";
+      const API_URL = "https://finally-back-in-stock-staging-6563cbfb9731.herokuapp.com";
       const response = await fetch(`${API_URL}/api/subscriber`, {
         method: "POST",
         headers: {
