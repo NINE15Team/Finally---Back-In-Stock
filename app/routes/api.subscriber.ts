c436eed2f66d330c90893921fc5eac1dfea9a73a
khair ali <khair@nine-15.com>
2024-03-16 15:51:37 +0500

    working

--- app/routes/api.subscriber.ts
+++ app/routes/api.subscriber.ts
@@ -5,6 +5,8 @@ import { cors } from "remix-utils/cors";
 
 export const loader: LoaderFunction = async ({ request }) => {
     console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    let requstBody = await request.json();
+    console.log(requstBody);
     return await json({ hello: "world" },{
             headers: {
                 "Access-Control-Allow-Origin": "*",
