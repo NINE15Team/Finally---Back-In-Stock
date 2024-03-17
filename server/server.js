599568634955ba767765a6861226946403f6aa26
khair ali <khair@nine-15.com>
2024-03-17 14:38:42 +0500

    add exress server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,27 @@
-// import the app build created by `remix build`
-import build from "./build/index.js";
+const express = require("express");
+var cors = require('cors')
+const { createRequestHandler } = require("@remix-run/express");
 
-// an express http server
-const app = express();
+let app = express();
 
-// and here your Remix app is "just a request handler"
-app.all("*", createRequestHandler({ build }));
+// Responses should be served with compression to minimize total network bytes.
+// However, where this compression happens can vary wildly depending on your stack
+// and infrastructure. Here we are compressing all our Express responses for the
+// purpose of this starter repository, but feel free to (re)move it or change it.
+app.use(cors());
 
-app.get('/products/', cors(), function (req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for an allowed domain.' })
+app.use(express.static("public"));
+
+
+app.all(
+    "*",
+    createRequestHandler({
+        build: require("./build"),
+    })
+);
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Express server started on http://localhost:${port}`);
 });
-
-app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-})
-// This is pseudo code, but illustrates what adapters do:
-export function createRequestHandler({ build }) {
-    console.log("%%%%%%%%%%%", build);
-    // returns an express.js specific handler for the express server
-    return async (req, res) => {
-        console.log(req);
-        // adapts the express.req to a Fetch API request
-        return req;
-    };
-}
