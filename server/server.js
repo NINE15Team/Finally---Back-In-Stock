07101e8c5c2c56af46213b3c5a806e16177735b8
khair ali <khair@nine-15.com>
2024-03-17 14:14:02 +0500

    add exress server

--- server/server.js
+++ server/server.js
@@ -5,10 +5,7 @@ const remix = require("@remix-run/express");
 
 const app = express();
 
-// Configure CORS for a specific domain
-const corsOptions = {
-    origin: '*',
-};
+app.use(cors(corsOptions));
 
 // Serve the Remix app
 app.all(
@@ -19,10 +16,10 @@ app.all(
 );
 
 
-app.get('/products', cors(), function (req, res, next) {
-    res.json({ msg: 'This is CORS-enabled for an allowed domain.' })
-})
+// app.get('/products', cors(), function (req, res, next) {
+//     res.json({ msg: 'This is CORS-enabled for an allowed domain.' })
+// })
 
-app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(80, function () {
+//     console.log('CORS-enabled web server listening on port 80')
+// })
