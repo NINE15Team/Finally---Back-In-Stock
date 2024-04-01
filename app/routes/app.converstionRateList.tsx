97db2459748d4fc1d72909150f10688d4414a487
khair ali <khair@nine-15.com>
2024-04-01 19:04:01 +0500

    working

--- app/routes/app.converstionRateList.tsx
+++ app/routes/app.converstionRateList.tsx
@@ -2,6 +2,7 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import {
     useActionData,
     useLoaderData,
+    useRevalidator,
 } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
 import { findAllNotificationHistory, getConversionRate } from "../services/notification-history.service";
@@ -34,24 +35,39 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 export default function Index() {
     const { data } = useLoaderData<typeof loader>();
     const actionData = useActionData<typeof action>();
+    let { revalidate } = useRevalidator();
     let rows: any = [];
     for (let i = 0; i < data.length; i++) {
         const item = data[i];
+        let totalOrders = item.completedCount || 0;
+        let totalViews = item.viewCount || 0;
+        let conversionRate = (totalOrders / totalViews) * 100;
         rows.push([
             item.createdAt,
             item.noOfNotifications,
-            item.viewCount,
+            totalViews,
             item.addToCartCount,
-            item.completedCount
+            totalOrders,
+            parseFloat(conversionRate.toFixed(2))
         ]);
     }
+
+    const refreshData = async () => {
+        revalidate();
+    };
 
 
     return (
         <Page>
+            <ui-title-bar title="Finally Back In Stock">
+                <button variant="primary" onClick={refreshData}>
+                    Reload Data
+                </button>
+            </ui-title-bar>
+
             <DataTable
                 columnContentTypes={["text", "text", "text", "text"]}
-                headings={["Date", "Notifications", "View", "Add to Cart", "Complete"]}
+                headings={["Date", "Notifications", "View", "Add to Cart", "Order Placed", "Conversion Rate"]}
                 rows={rows}
                 showTotalsInFooter
                 pagination={{
