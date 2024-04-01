93a7db44b44c2b85c45d345674f9649a1a3f3edf
B-Fabrice <bizimanafabrice707@gmail.com>
2024-04-02 00:40:16 +0200

    dev root index

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,42 +1,44 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import {
   useActionData,
-  useNavigation,
-  useSubmit,
   useLoaderData,
-  useRevalidator,
-  json,
-  Form,
-  ClientActionFunctionArgs,
 } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
-import { findAllSubscribers, findTotalPotentialRevenue } from "../services/customer-subscriber.service";
+import { countOfSubscribers,  } from "../services/customer-subscriber.service";
 import { findSubscribedProducts } from "../services/product-info.service";
 import { upsertEmail } from "../services/email.service";
-import { updateStoreInfo, isInitilized, getStoreInfoShopify, activateWebPixel } from "../services/store-info.service";
-import Instructions from "./app.instructions";
-import SubscriberList from "./app.subscriberList";
+import { updateStoreInfo, isInitilized, getStoreInfoShopify } from "../services/store-info.service";
 import { useState } from "react";
+
+import { Layout, Page } from "@shopify/polaris";
+import Checklist from "~/components/checklist";
+import NumRequest from "~/components/num-request";
+import Request from "~/components/request";
+import { sumNoOfNotifications } from "~/services/notification-history.service";
+
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   let { admin, session } = await authenticate.admin(request);
   let initilized = await isInitilized(admin);
-  if (false && !initilized) {
-    await activateWebPixel(admin);
+  let { id, myshopify_domain, name, email }: any = await getStoreInfoShopify(admin);
+  if (!initilized) {
+    // await activateWebPixel(admin);
+    await upsertEmail({
+      storeId: id,
+      shopifyURL: myshopify_domain,
+      title: name,
+      senderEmail: email
+    });
+    let shopInfo: any = await updateStoreInfo(admin);
   }
-  let shopInfo: any = await getStoreInfoShopify(admin);
-  const data = await findSubscribedProducts({ shopifyURL: shopInfo.myshopify_domain });
-  const data2 = await findAllSubscribers({
-    shopifyURL: shopInfo.myshopify_domain,
-    isNotified: false,
-  });
-  const { potentialRevenue } = await findTotalPotentialRevenue(shopInfo.myshopify_domain);
-  return { data, data2, shopifyURL: shopInfo.myshopify_domain, storeName: shopInfo.name, potentialRevenue, initilized };
+  const subscribedProducts = await findSubscribedProducts({ shopifyURL: myshopify_domain });
+  const totalNotifications = await sumNoOfNotifications(myshopify_domain);
+  const newSubscribers = await countOfSubscribers(myshopify_domain);
+  return { subscribedProducts, totalNotifications, newSubscribers, shopifyURL: myshopify_domain, storeName: name, initilized };
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   let { admin, session } = await authenticate.admin(request);
-  let formData = await request.formData();
   let shopInfo: any = await updateStoreInfo(admin);
   await upsertEmail({
     storeId: shopInfo.id,
@@ -44,7 +46,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     title: shopInfo.name,
     senderEmail: shopInfo.email
   });
-  await activateWebPixel(admin);
+  // await activateWebPixel(admin);
   return await isInitilized(admin);
 };
 
@@ -52,18 +54,15 @@ export default function Index() {
   const actionData = useActionData<typeof action>();
   let { initilized } = useLoaderData<typeof loader>();
   const [appInit, setAppInit] = useState(initilized);
-  const renderContent = () => {
-    if (initilized) {
-      return <SubscriberList />;
-    } else {
-      return <Instructions showButton={true} />;
-    }
-  };
 
 
   return (
-    <>
-      {renderContent()}
-    </>
+    <Page>
+      <Layout>
+        <Checklist />
+        <NumRequest />
+        <Request />
+      </Layout>
+    </Page>
   );
 }
