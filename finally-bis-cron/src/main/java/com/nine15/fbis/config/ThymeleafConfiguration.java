baff771f9fe70fd2c12dfd2702d6145c04b22a73
khair ali <khair@nine-15.com>
2024-04-04 03:31:24 +0500

    remove webmvc

--- finally-bis-cron/src/main/java/com/nine15/fbis/config/ThymeleafConfiguration.java
+++ finally-bis-cron/src/main/java/com/nine15/fbis/config/ThymeleafConfiguration.java
@@ -2,30 +2,28 @@ package com.nine15.fbis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
-@EnableWebMvc
 public class ThymeleafConfiguration {
 
-    @Bean
-    public ClassLoaderTemplateResolver templateResolver() {
-        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
-        resolver.setPrefix("templates/"); // Location of thymeleaf template
-        resolver.setCacheable(false); // Turning of cache to facilitate template changes
-        resolver.setSuffix(".html"); // Template file extension
-        resolver.setTemplateMode("HTML"); // Template Type
-        resolver.setCharacterEncoding("UTF-8");
-        return resolver;
-    }
+  @Bean
+  public ClassLoaderTemplateResolver templateResolver() {
+    ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+    resolver.setPrefix("templates/"); // Location of thymeleaf template
+    resolver.setCacheable(false); // Turning of cache to facilitate template changes
+    resolver.setSuffix(".html"); // Template file extension
+    resolver.setTemplateMode("HTML"); // Template Type
+    resolver.setCharacterEncoding("UTF-8");
+    return resolver;
+  }
 
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver());
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine engine = new SpringTemplateEngine();
+    engine.setTemplateResolver(templateResolver());
 
-        return engine;
-    }
+    return engine;
+  }
 }
