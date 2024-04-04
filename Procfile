e87a21663019f3d648c18062c77e95f4a1a6e90e
khair ali <khair@nine-15.com>
2024-04-04 06:29:11 +0500

    default proc

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: npm start
-worker: cd finally-bis-cron && java -Dserver.port=$PORT $JAVA_OPTS -jar target/*.jar
--- finally-bis-cron/src/main/java/com/nine15/fbis/service/NotificationHistoryService.java
+++
@@ -1,9 +0,0 @@
-package com.nine15.fbis.service;
-
-import com.nine15.fbis.entity.NotificationHistoryEntity;
-
-public interface NotificationHistoryService {
-
-    void save(NotificationHistoryEntity notificationHistoryEntity);
-
-}
